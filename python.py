97578ebbcbc402d55247ac8b7efb65c2506cd65a
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-03-28 22:57:03 +0530

    Update python.py

--- python.py
+++ python.py
@@ -21,3 +21,7 @@ ques 4
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 evens = list(filter(lambda x: x % 2 == 0, nums))
 print(evens)
+
+ques 4
+student = {"name": "Leo", "age": 22, "major": "CS"}
+print(len(student))
