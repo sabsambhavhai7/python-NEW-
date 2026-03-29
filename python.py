fe1e54a73288c68641d89ba7fe7999a30271ea89
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-03-29 23:21:25 +0530

    Update python.py

--- python.py
+++ python.py
@@ -73,3 +73,13 @@ QUES 4
 colors = ["red", "blue", "red", "green", "red", "blue"]
 count_red = colors.count("red")
 print(f"Red appears {count_red} times.")
+
+QUES 5
+class Dog:
+    def __init__(self, name):
+        self.name = name
+    def bark(self):
+        return f"{self.name} says Woof!"
+
+my_dog = Dog("Buddy")
+print(my_dog.bark())
