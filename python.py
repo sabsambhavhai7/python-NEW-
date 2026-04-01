ffbf9fd5ff2bec882229da4ef8cac97d3c0e07af
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-04-01 22:30:20 +0530

    Update python.py

--- python.py
+++ python.py
@@ -134,4 +134,8 @@ nested_list = [[1, 2], [3, 4], [5, 6]]
 flat_list = [item for sublist in nested_list for item in sublist]
 print(f"Flattened list: {flat_list}")
 
+ques 2
+fruits = ["Apple", "Banana", "Cherry"]
+for index, fruit in enumerate(fruits, start=1):
+    print(f"Rank {index}: {fruit}")
 
