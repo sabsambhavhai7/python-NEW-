b66496e993fdbf549bdc4b4c101d4211df691273
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-04-04 23:00:02 +0530

    Update python.py

--- python.py
+++ python.py
@@ -270,3 +270,11 @@ from collections import namedtuple
 Point = namedtuple('Point', ['x', 'y'])
 p = Point(10, 20)
 print(f"Coordinates: x={p.x}, y={p.y}")
+
+
+ques 2
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 5, 6]
+intersection = [x for x in list1 if x in list2]
+print(f"Intersection: {intersection}")
+
