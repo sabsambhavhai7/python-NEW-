fa66868fa6164a7a4c21dcb329f0b2821a056524
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-04-11 23:57:35 +0530

    Update python.py

--- python.py
+++ python.py
@@ -811,6 +811,12 @@ most_common_val, count = Counter(data).most_common(1)[0]
 print(f"Value {most_common_val} appears {count} times.")
 
 
+ques 8
+import base64
+text = "Python101"
+encoded = base64.b64encode(text.encode())
+print(f"Encoded: {encoded.decode()}")
+
 ques 9
 
 
