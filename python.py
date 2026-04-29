1cda750e0f4620219ab59ed912cb1c40212a8ae3
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-04-29 13:50:39 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1470,3 +1470,26 @@ ________________________________________________________________________________
 Date 26 april 2026
 
 ques 1
+
+
+_________________________________________________________________________________________________________________________________________________
+
+date 29 april 2026 
+
+ques 1 
+import functools
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with {args}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned {result}")
+        return result
+    return wrapper
+
+@debug
+def add(a, b):
+    return a + b
+
+add(5, 10)
