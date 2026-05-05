6681ca687c223e91299a2a386ec3c0217fed82ed
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-05-05 23:57:50 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2068,3 +2068,11 @@ scores = {"Alice": 90, "Bob": 95, "Charlie": 85}
 # Sort by value (descending)
 sorted_scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
 print(sorted_scores)
+
+
+ques 8 
+import itertools
+
+colors = itertools.cycle(["Red", "Green", "Blue"])
+for i in range(5):
+    print(next(colors)) # Red -> Green -> Blue -> Red -> Green
